ba54274fe4c952f2d962fffd8446704cc0468532
Kevin Olasava <113872853+elkevino6@users.noreply.github.com>
2024-03-29 13:26:52 -0700

    Add files via upload
    Folder contains a Mozilla Monitor Ad, and a Mozilla VPN Ad, these are chat pop ups, need to add effects and timers though, everything else works though

---
+++ Amo-Chat-popup-ads/responsive-chat-popup-mozilla-monitor/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+  // Show chat popup when document is ready
+  $('#chat-popup').show();
+
+  // Close chat popup when close button is clicked
+  $('#close-chat').click(function(){
+    $('#chat-popup').hide();
+  });
+
+
+});
